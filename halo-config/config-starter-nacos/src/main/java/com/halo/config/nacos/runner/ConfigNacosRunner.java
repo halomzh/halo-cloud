922aed325038006fc23c24548752b660f6bc274c
mzh <70004810+halomzh@users.noreply.github.com>
2021-04-01 12:56:28 +0800

    修改配置runner

--- halo-config/config-starter-nacos/src/main/java/com/halo/config/nacos/runner/ConfigNacosRunner.java
+++ halo-config/config-starter-nacos/src/main/java/com/halo/config/nacos/runner/ConfigNacosRunner.java
@@ -1,18 +1,19 @@
 package com.halo.config.nacos.runner;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
 import com.alibaba.cloud.nacos.NacosConfigProperties;
+import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.config.ConfigType;
-import com.alibaba.nacos.client.config.NacosConfigService;
 import com.halo.config.nacos.configuration.properties.ConfigNacosProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author shoufeng
@@ -20,8 +21,9 @@ import java.io.File;
 
 @Slf4j
 public class ConfigNacosRunner implements ApplicationRunner {
+
 	@Autowired
-	private NacosConfigService nacosConfigService;
+	private NacosConfigManager nacosConfigManager;
 
 	@Autowired
 	private NacosConfigProperties nacosConfigProperties;
@@ -32,6 +34,7 @@ public class ConfigNacosRunner implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		String group = nacosConfigProperties.getGroup();
+		ConfigService configService = nacosConfigManager.getConfigService();
 		for (String configFileName : configNacosProperties.getConfigFileNameList()) {
 			String[] configFileNameSplitStrings = configFileName.split("\\.");
 			ConfigType configType = ConfigType.valueOf(configFileNameSplitStrings[configFileNameSplitStrings.length - 1].toUpperCase());
@@ -39,13 +42,17 @@ public class ConfigNacosRunner implements ApplicationRunner {
 				log.error("上传配置[{}]失败: 未知配置类型", configFileName);
 				continue;
 			}
-			File configFile = ResourceUtils.getFile(configFileName);
-			if (ObjectUtils.isEmpty(configFile)) {
+			InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(configFileName);
+			if (ObjectUtils.isEmpty(resourceAsStream)) {
 				log.error("上传配置[{}]失败: 获取配置文件失败", configFileName);
 				continue;
 			}
-			String configStr = FileUtils.readFileToString(configFile);
-			nacosConfigService.publishConfig(configFileName, group, configStr, configType.getType());
+			String configStr = IOUtils.toString(resourceAsStream, String.valueOf(StandardCharsets.UTF_8));
+			if (configService.publishConfig(configFileName, group, configStr, configType.getType())) {
+				log.info("上传配置[{}]成功", configFileName);
+				continue;
+			}
+			log.warn("上传配置[{}]失败", configFileName);
 		}
 
 	}
